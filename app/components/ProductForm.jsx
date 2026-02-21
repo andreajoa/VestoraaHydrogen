2b4c4751fe3d6af567ffa8415d74c008245f04da
Andre Almeida <andrealmeida@Andres-MacBook-Air.local>
2026-02-21 12:46:03 -0300

    fix: React is not defined - use useState not React.useState

--- app/components/ProductForm.jsx
+++ app/components/ProductForm.jsx
@@ -46,7 +46,7 @@ export function ProductForm({ productOptions, selectedVariant }) {
   const sizeOption = productOptions.find(o => ['Size','size'].includes(o.name));
   const otherOptions = productOptions.filter(o => !['Color','Colour','color','colour','Size','size'].includes(o.name));
   const selectedColorValue = colorOption?.optionValues?.find(v => v.selected);
-  const [userSelectedSize, setUserSelectedSize] = React.useState(false);
+  const [userSelectedSize, setUserSelectedSize] = useState(false);
   const selectedSizeValue = sizeOption?.optionValues?.find(v => v.selected);
   const displaySize = userSelectedSize ? selectedSizeValue : null;
 
